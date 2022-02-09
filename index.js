816576bedf1e0f82471c2761787efdeccb52bb8c
Rene Hernandez <rene83120@gmail.com>
2022-02-08 17:51:13 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names) {
+  let newNames = [];
+  for (let i = 0; i < names.length; i++) {
+    console.log([`Thank you, ${names[i]}, for the wonderful surprise gift!`]);
+    newNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return newNames;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
